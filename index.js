e2d702d9fef04fc6353bf14db88f9fc561d35218
Natalie Faciane <nataliefaciane2015@gmail.com>
2026-04-27 13:09:50 -0500

    some css

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 $(() => {
   const $page = $('#all-contents');
 
-  const $tweetsDiv = $('<div class="tweets"></div>');
+  //position all-text on page
+  $page.css('margin-top', '200px');
+
+  const $tweetsDiv = $('<div class="tweets"></div>').css('margin-top', '120px');
 
   $page.append($tweetsDiv);
 
   //put this code into a function
   function addNewTweets(tweets) {
-    const $tweets = tweets.map((tweet) => {
+    const $tweets = tweets.slice(0, 10).map((tweet) => {
       const $tweet = $('<div class="tweet"></div>');
       //username needs to be in its own tag
       //swapping const text = `@${tweet.user}: ${tweet.message}`;
       //setting each class
-      const $username = $('<p class="username">').text(tweet.user);
-      const $message = $('<p class="message">').text(tweet.message);
-      const $timestamp = $('<p class="timestamp">').text(moment(tweet.created_at).format('MMMM Do YYYY, h:mm:ss a'));
-      const $humanFriendlyTimestamp = $('<p class="humanFriendlyTimestamp">').text(moment(tweet.created_at).fromNow());
+      const $username = $('<span class="username">').text(tweet.user);
+      const $message = $('<span class="message">').text(tweet.message);
+      const $timestamp = $('<span class="timestamp">').text(moment(tweet.created_at).format('MMMM Do YYYY, h:mm:ss a'));
+      const $humanFriendlyTimestamp = $('<span class="humanFriendlyTimestamp">').text(moment(tweet.created_at).fromNow());
+
+      const $tweetStructure = $('<p>').append($('<span>').text('@')).append($username).append($('<p>')).append($message).append(' - ').append($timestamp).append(' (').append($humanFriendlyTimestamp).append(')');
 
       //give the username it's click handler function inside here
-      $tweet.append($username).append($message).append($timestamp).append($humanFriendlyTimestamp);
+      $tweet.append($tweetStructure);
 
       return $tweet;
     });
@@ -30,8 +35,10 @@ $(() => {
   addNewTweets(streams.home);
 
   //allow users to write their own tweets, appending objects to page
-  const $usernameInput = $('<input id="username-input">');
-  const $messageInput = $('<input id="message-input">');
+  const $usernameInput = $('<textarea id="username-input">');
+  const $messageInput = $('<textarea id="message-input">').css(
+    //style text box
+    {width: '500px', height: '120px', marginTop: '20px', display: 'block', backgroundColor: '2a2a2a', color: '#e0e0e0', boarder: '1px solid #444'});
   const $submit = $('<input type="submit">');
   $page.prepend($submit).prepend($messageInput).prepend($usernameInput);
 
@@ -55,19 +62,37 @@ $(() => {
 
   //auto adding new tweets to the top of the home page
   let lastCount = streams.home.length;
-  setInterval(function () {
+
+  function loadNewTweets() {
     let newTweets = streams.home.slice(lastCount);
     newTweets.forEach(function (tweet) {
       const $tweet = $('<div class="tweet"></div>');
-      const $username = $('<p class="username">').text(tweet.user);
-      const $message = $('<p class="message">').text(tweet.message);
-      const $timestamp = $('<p class="timestamp">').text(moment(tweet.created_at).format('MMMM Do YYYY, h:mm:ss a'));
-      const $humanFriendlyTimestamp = $('<p class="humanFriendlyTimestamp">').text(moment(tweet.created_at).fromNow());
-      $tweet.append($username).append($message).append($timestamp).append($humanFriendlyTimestamp);
+      const $username = $('<span class="username">').text(tweet.user);
+      const $message = $('<span class="message">').text(tweet.message);
+      const $timestamp = $('<span class="timestamp">').text(moment(tweet.created_at).format('MMMM Do YYYY, h:mm:ss a'));
+      const $humanFriendlyTimestamp = $('<span class="humanFriendlyTimestamp">').text(moment(tweet.created_at).fromNow());
+
+      const $tweetStructure = $('<p>').append($('<span>').text('@')).append($username).append($('<p>')).append($message).append(' - ').append($timestamp).append(' (').append($humanFriendlyTimestamp).append(')');
+
+      //give the username it's click handler function inside here
+      $tweet.append($tweetStructure);
+
+      if ($('.tweet').length > 10) {
+        $('.tweet').last().remove();
+      }
+
       $tweetsDiv.prepend($tweet);
     });
     lastCount = streams.home.length;
-  }, 1500);
+  }
+//add refresh button; label: "New Tweet"; position: just above the tweets on left-hand side
+  const $newTweetsButton = $('<button id="new-tweets-button">New Tweets</button>').css('display', 'block');
+//position: just above the tweets
+  $tweetsDiv.before($newTweetsButton);
+
+  $newTweetsButton.on('click', loadNewTweets);
+
+  setInterval(loadNewTweets, 1500);
 
 });
 
